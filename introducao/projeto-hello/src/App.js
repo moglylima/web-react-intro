7497b525a9144846eeeed5ac2b3a98092f776c1e
MichelSouzaGit <michels.git@gmail.com>
2022-12-13 20:44:03 -0300

    Atualizando o repositório, adicionando novamente o projeto de Hello World

---
+++ introducao/projeto-hello/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <div className="App">
+
+     {/* <h1>
+       Hello World...
+     </h1> */}
+
+     <HelloWorld />
+    
+    </div>
+  );
+}
+
+export default App;
